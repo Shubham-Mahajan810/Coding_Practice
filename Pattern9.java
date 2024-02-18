67b951c0e712feb37b792a04417b4a7a0f1cf1ef
Shubham Mahajan <131528208+Shubham-Mahajan810@users.noreply.github.com>
2024-02-18 10:03:11 +0530

    Add files via upload

---
+++ Pattern9.java
@@ -0,0 +1,20 @@
+/* 
+     *
+    *
+   *
+  *
+ *
+ 
+ */
+
+public class Pattern9 {
+    public static void main(String arge[]){
+        for(int i=1;i<=5;i++){
+            for(int j=5;j>=i;j--){
+                System.out.print(" ");
+            }
+           
+         System.out.println("*");
+        }
+    }
+}
